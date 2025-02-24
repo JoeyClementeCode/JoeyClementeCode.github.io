7ff9597e0560ea33c44ad59fd29ef88c8c26cb17
JoeyClementeCode <joewclemente@gmail.com>
2025-02-23 18:28:49 -0800

    Push new project page start

---
+++ script.js
@@ -0,0 +1,11 @@
+const videos = document.querySelector(".project video");
+
+videos.forEach(video => {
+  video.addEventListener('mouseenter', () => {
+    video.play();
+  });
+  
+  video.addEventListener('mouseleave', () => {
+    video.pause();
+  });
+});
